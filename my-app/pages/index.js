4012fc1ce347305449c5668d34096cb9508003ee
cadu <cadu@2km.com.br>
2022-04-26 20:58:23 +0100

    fix: Changed link name property

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -196,7 +196,7 @@ export default function Home() {
         <meta name="description" content="Whitelist-Dapp" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;500;700&display=swap" rel="stylesheet" />
 
 
